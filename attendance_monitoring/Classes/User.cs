931651d8d1dace27b37b565d721dba7155fffdb4
Neil Vallecer <neilvallecer12@gmail.com>
2025-08-04 20:32:52 +0800

    refactor: Replace custom auth with ASP.NET Core Identity + JWT; remove custom auth files; add in-memory EF and Swagger JWT config

--- attendance_monitoring/Classes/User.cs
+++
@@ -1,7 +0,0 @@
-namespace attendance_monitoring.Classes;
-
-public class User
-{
-    public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-}
--- attendance_monitoring/IRepository/ILoginRepository.cs
+++
@@ -1,9 +0,0 @@
-using attendance_monitoring.Models.Response;
-
-namespace attendance_monitoring.Repositories;
-
-public interface ILoginRepository
-{
-    public Task<LoginModel> GetLogin();
-    public Task<object> Register();
-}
