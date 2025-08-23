d085a2a044615400c1cb51699a5ceedc2352e431
Neil Vallecer <neilvallecer12@gmail.com>
2025-08-23 18:10:00 +0800

    feat: created initial section model with reference to the Instructor table

---
+++ attendance_monitoring/Classes/Section.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace attendance_monitoring.Classes;
+
+public class Section
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    
+    // Foreign key to Instructor - should not be nullable
+    public int InstructorId { get; set; }
+    
+    // Navigation property - required relationship
+    [ForeignKey("InstructorId")]
+    public Instructor Instructor { get; set; } = null!;
+    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
