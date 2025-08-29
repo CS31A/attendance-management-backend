dab2d310ba98cacb717a27375934c44dc7909192
Neil Vallecer <neilvallecer12@gmail.com>
2025-08-29 22:07:49 +0800

    refactor: remove unused DTOs and example endpoint
    - removed unused LoginModel and UserModel
    
    - deleted the boilerplate weather forecast endpoint

--- attendance_monitoring/Models/DTO/LoginModel.cs
+++
@@ -1,9 +0,0 @@
-namespace attendance_monitoring.Models.DTO;
-
-public class LoginModel
-{
-    public int ID { get; set; }
-    public string? FullName { get; set; }
-    public string? UserName { get; set; }
-    public string? Email { get; set; }
-}
--- attendance_monitoring/Models/DTO/UserModel.cs
+++
@@ -1,7 +0,0 @@
-namespace attendance_monitoring.Models.DTO;
-
-public class UserModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
