29af2de057517f859cf7fb5a2273e8b850a1b6ad
Neil Vallecer <neilvallecer12@gmail.com>
2025-09-04 17:04:26 +0800

    refactor: initialize dbset properties with null-forgiving operator and corrected some issues int the instructor service
    - added null-forgiving operator (!) to DbSet properties in AppDbContext
    to address nullable reference types
    
    - updated GetInstructorByIdAsync method and interface to properly handle
    null returns
    
    - improved null safety in InstructorService implementation

--- attendance_monitoring/IServices/IInstructorService.cs
+++ attendance_monitoring/IServices/IInstructorService.cs
@@ -9,7 +9,7 @@ namespace attendance_monitoring.IServices;
 public interface IInstructorService
 {
     Task<IEnumerable<Instructor>> GetAllInstructorsAsync(PaginationQuery paginationQuery);
-    Task<Instructor> GetInstructorByIdAsync(int id);
+    Task<Instructor?> GetInstructorByIdAsync(int id);
     Task<(Instructor, string)> CreateInstructorAsync(CreateInstructor createInstructor, ClaimsPrincipal user);
     Task<(Instructor, string)> UpdateInstructorAsync(int id, UpdateInstructor updateInstructor, ClaimsPrincipal user);
 }
