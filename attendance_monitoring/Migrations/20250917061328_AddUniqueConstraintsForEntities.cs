abfd6f937a1efb9a582cd8a20b0e55a36cc57951
Neil Vallecer <neilvallecer12@gmail.com>
2025-09-17 15:37:44 +0800

    refactor: added unique constraints to entities and update seciton service
    - added unique constraints to Admin, Classroom, Course, Instructor,
    RefreshToken, Section, Student, and Subject entities
    - updated SectionService to remove unused ApplicationDbContext
    dependency
    - generated migration file to apply unique constraints to existing
    database

---
+++ attendance_monitoring/Migrations/20250917061328_AddUniqueConstraintsForEntities.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace attendance_monitoring.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintsForEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Students_UserId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Instructors_UserId",
+                table: "Instructors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects",
+                column: "Code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_Name",
+                table: "Subjects",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_UserId",
+                table: "Students",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sections_Name",
+                table: "Sections",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instructors_UserId",
+                table: "Instructors",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_Name",
+                table: "Courses",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classrooms_Name",
+                table: "Classrooms",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_Code",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subjects_Name",
+                table: "Subjects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_UserId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sections_Name",
+                table: "Sections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Instructors_UserId",
+                table: "Instructors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Courses_Name",
+                table: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Classrooms_Name",
+                table: "Classrooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_UserId",
+                table: "Students",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Instructors_UserId",
+                table: "Instructors",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserId",
+                table: "Admins",
+                column: "UserId");
+        }
+    }
+}
