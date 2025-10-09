f56afc16f77088218878feebcb7cb93b6e530bd0
Neil Vallecer <neilvallecer12@gmail.com>
2025-10-09 14:59:20 +0800

    refactor: remove unused serialization code from schedule exceptions
    - remove dead obsolete doce bro

--- attendance_monitoring/Exceptions/ScheduleServiceException.cs
+++ attendance_monitoring/Exceptions/ScheduleServiceException.cs
@@ -1,5 +1,3 @@
-using System.Runtime.Serialization;
-
 namespace attendance_monitoring.Exceptions
 {
     public class ScheduleServiceException : Exception
@@ -26,10 +24,6 @@ namespace attendance_monitoring.Exceptions
         public ScheduleServiceException(string? errorCode, string? message, Exception? innerException) : base(message, innerException)
         {
             ErrorCode = errorCode;
-        }
-
-        protected ScheduleServiceException(SerializationInfo info, StreamingContext context) : base(info, context)
-        {
         }
     }
 }
