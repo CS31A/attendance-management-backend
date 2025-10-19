33ad8f16d2b7767866da1f919cd5362b4515d18e
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-10-19 20:38:38 +0800

    feat(instructor): implemented endpoint to get all subjects for an instructor
    - added a new GET endpoint in InstructorController
    - Implemented GetSubjectsByInstructorIdAsync method in Instructor
    Service
    - created a new SubjectResponseDto to define the shape of the response

--- attendance_monitoring/IServices/IInstructorService.cs
+++ attendance_monitoring/IServices/IInstructorService.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using attendance_monitoring.Classes;
 using attendance_monitoring.Models.DTO.Request;
+using attendance_monitoring.Models.DTO.Response;
 
 namespace attendance_monitoring.IServices;
 
@@ -8,6 +9,7 @@ public interface IInstructorService
 {
     Task<IEnumerable<Instructor>> GetAllInstructorsAsync();
     Task<Instructor> GetInstructorByIdAsync(int id);
+    Task<IEnumerable<SubjectResponseDto>> GetSubjectsByInstructorIdAsync(int instructorId);
     Task<(Instructor?, string?)> CreateInstructorAsync(CreateInstructor createInstructor, ClaimsPrincipal user);
     Task<Instructor> UpdateInstructorAsync(int id, UpdateInstructor updateInstructor, ClaimsPrincipal user);
     Task SoftDeleteInstructorAsync(int id, ClaimsPrincipal user);
