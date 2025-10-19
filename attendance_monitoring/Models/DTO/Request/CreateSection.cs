26645af8dda0326dac2fd5a5e08bf18da0cd2930
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-10-19 12:00:20 +0800

    feat(section): remove direct instructor relationship

--- attendance_monitoring/Models/DTO/Request/CreateSection.cs
+++ attendance_monitoring/Models/DTO/Request/CreateSection.cs
@@ -8,9 +8,6 @@ namespace attendance_monitoring.Models.DTO.Request
         [StringLength(100, ErrorMessage = "Section name must be between 1 and 100 characters", MinimumLength = 4)]
         public string Name { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Instructor ID is required")]
-        public int InstructorId { get; set; }
-
         [Required(ErrorMessage = "Course ID is required")]
         public int CourseId { get; set; }
     }
