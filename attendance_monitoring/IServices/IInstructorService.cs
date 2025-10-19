b31a950e89fa47554f92c2df43665d74737a7ea5
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-10-19 22:55:19 +0800

    feat(instructor): implemented instructor profile endpoint

--- attendance_monitoring/IServices/IInstructorService.cs
+++ attendance_monitoring/IServices/IInstructorService.cs
@@ -10,6 +10,7 @@ public interface IInstructorService
     Task<IEnumerable<Instructor>> GetAllInstructorsAsync();
     Task<Instructor> GetInstructorByIdAsync(int id);
     Task<IEnumerable<SubjectResponseDto>> GetSubjectsByInstructorIdAsync(int instructorId);
+    Task<InstructorProfileResponseDto?> GetInstructorProfileAsync(ClaimsPrincipal user);
     Task<(Instructor?, string?)> CreateInstructorAsync(CreateInstructor createInstructor, ClaimsPrincipal user);
     Task<Instructor> UpdateInstructorAsync(int id, UpdateInstructor updateInstructor, ClaimsPrincipal user);
     Task SoftDeleteInstructorAsync(int id, ClaimsPrincipal user);
