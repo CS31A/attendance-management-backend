df871536ca9f449a06d9a1bb2bb5b6eafe875ede
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-10-21 11:30:18 +0800

    feat(schedule): include related entity data in schedule responses
    - created ClassroomResponseDto, InstructorResponseDto, and
    ScheduleResponseDto
    - add eager loading for all related entities in ScheduleRepo
    - create d helper method (MapToResponseDto)
    - update get endponits to return ScheduleResponseDto with nested entity
    data
    - change FindAsync to FirstOrDefaultAsync for proper eager loading
    support

---
+++ attendance_monitoring/Models/DTO/Response/InstructorResponseDto.cs
@@ -0,0 +1,10 @@
+namespace attendance_monitoring.Models.DTO.Response
+{
+    public class InstructorResponseDto
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public string? Email { get; set; }
+    }
+}
